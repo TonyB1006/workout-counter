c19728cc035451c90eb52668426956ed74aa81fa
TonyB1006 <159938105+TonyB1006@users.noreply.github.com>
2024-03-07 09:47:39 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+let saveEl = document.getElementById("save-el");
+let countEl = document.getElementById("count-el");
+let resetEl = document.getElementById("count-el");
+let count = 0;
+let set = 0;
+
+function add() {
+    count += 1;
+    countEl.textContent = count;
+}
+
+function reset() {
+    count = 0;
+    countEl.textContent = count;
+}
+
+function save() {
+    set += 1;
+    let countStr = "Set " + set + ": " + count + " reps";
+    saveEl.innerHTML += "<br>" + countStr;
+    countEl.textContent = 0
+    count = 0
+}
+
+function removeData() {
+    saveEl.innerHTML = "";
+    saveEl.textContent = "Previous Sets:"
+}
+
+
+
